7e09640e177c3ebbde716a843bba9d9a74b23ee2
sebastian <7sebastianbo11@gmail.com>
2026-07-08 19:58:21 -0400

    Add bomb generation (created a new bug with flickering)

--- constants.hpp
+++ constants.hpp
@@ -3,5 +3,7 @@
 constexpr inline int SCREEN_WIDTH_TILES { 30 };
 constexpr inline int SCREEN_HEIGHT_TILES { 30 };
 
+constexpr inline int TILE_COUNT { SCREEN_HEIGHT_TILES * SCREEN_WIDTH_TILES };
+
 constexpr inline int NUMBER_OFFSET { 8 };
 constexpr inline int TILE_WIDTH { 30 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "raylib.h"
+#include "rlgl.h"
 
 #include <iostream>
 #include <array>
+
+#include "random.hpp"
 
 #include "constants.hpp"
 #include "draw.hpp"
@@ -26,8 +29,12 @@ void callDrawFunction(TileState state, int startX, int startY) {
 
     switch (state) {
     case base:
+        // this case is here to make sure we do nothing (in case it is ever called)
         return;
     case bomb:
+        // drawBomb(int startX, int startY); doesnt need to be called since bombs should be hidden
+        // drawNine(startX, startY);
+        DrawRectangle(startX * TILE_WIDTH, startY * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH, Color { ORANGE });
         return;
     case flag:
         drawFlag(startX, startY);
@@ -38,32 +45,53 @@ void callDrawFunction(TileState state, int startX, int startY) {
     }
 }
 
+int addBombsToGrid(std::array<std::array<TileState, SCREEN_WIDTH_TILES>, SCREEN_HEIGHT_TILES>& grid) {
+    constexpr static double percentage { 0.2 };
+    int bombCount { static_cast<int>(percentage * TILE_COUNT) };
+
+    std::cout << bombCount << "\n";
+
+    
+    while (bombCount--) {
+
+        int idx { Random::get(0, TILE_COUNT - bombCount) };
+
+        for (auto& row : grid) {
+            for (auto& tile : row) {
+                if (tile != TileState::bomb) {
+                    idx--;
+                    if (idx < 1) {
+                        tile = TileState::bomb;
+                        goto end;
+                    }
+                }
+            }
+        }
+
+        end:
+
+    }
+
+    return bombCount;
+}
+
 int main() {
+    static std::array<std::array<TileState, SCREEN_WIDTH_TILES>, SCREEN_HEIGHT_TILES> grid {};
+
+    for (auto i { 0uz }; i < grid.size(); ++i) {
+        for (auto j { 0uz }; j < grid[i].size(); ++j) {
+            grid[i][j] = TileState::base;
+        }
+    }
+
+    addBombsToGrid(grid);
 
     InitWindow(SCREEN_WIDTH_TILES * TILE_WIDTH, SCREEN_HEIGHT_TILES * TILE_WIDTH, "My first window");
 
-    std::array<std::array<TileState, SCREEN_WIDTH_TILES>, SCREEN_HEIGHT_TILES> grid {{ }};
-
-
-    SetTargetFPS(30);
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        BeginDrawing();
-
-            ClearBackground(Color { GRAY });
-            
-
-            for (int i { }; i < SCREEN_HEIGHT_TILES; ++i) {
-                for (int j { }; j < SCREEN_WIDTH_TILES; ++j) {
-                    if (grid[j][i] != TileState::base) {
-                        callDrawFunction(grid[j][i], i, j);
-                    }
-                    
-                    DrawRectangleLines(j * TILE_WIDTH, i * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH, Color { BLACK });
-                }
-            }
-
-            if (IsMouseButtonPressed(1)) {
+        if (IsMouseButtonPressed(1)) {
                 int x { GetMouseX() / TILE_WIDTH };
                 int y { GetMouseY() / TILE_WIDTH };
 
@@ -71,8 +99,25 @@ int main() {
                     grid[y][x] = TileState::flag;
                 } else if (grid[y][x] == TileState::flag) {
                     grid[y][x] = TileState::base;
-                } else {
-                    throw std::runtime_error("Invalid case with flag");
+                }
+            }
+
+        BeginDrawing();
+
+            ClearBackground(GRAY);
+
+            DrawFPS(5, 5);
+            
+
+            for (int i { }; i < SCREEN_HEIGHT_TILES; ++i) {
+                for (int j { }; j < SCREEN_WIDTH_TILES; ++j) {
+                    DrawRectangle(i * TILE_WIDTH, j * TILE_WIDTH, TILE_WIDTH - 1, TILE_WIDTH - 1, BLACK);
+
+                    if (grid[i][j] != TileState::base) {
+                        callDrawFunction(grid[i][j], j, i);
+                    }
+                    
+                    // DrawRectangleLines(i * TILE_WIDTH, j * TILE_WIDTH, TILE_WIDTH - 1, TILE_WIDTH - 1, BLACK);
                 }
             }
 
